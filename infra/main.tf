a6aa8320214e851797d4a2e784bb1196e8feebe8
giano-montano <gianomontanoc@gmail.com>
2026-09-25 00:47:38 +0000

    inyección 2 que obliga a destruir

--- infra/main.tf
+++ infra/main.tf
@@ -64,7 +64,7 @@ resource "aws_ecr_repository_policy" "descarga_lambda" {
 # El nombre del grupo debe coincidir con el que Lambda usa por convencion,
 # o la funcion crea el suyo y esta retencion no aplica.
 resource "aws_cloudwatch_log_group" "funcion" {
-  name              = "/aws/lambda/${var.nombre_aplicacion}"
+  name              = "/aws/lambda/${var.nombre_aplicacion}-viejo"
   retention_in_days = 7
 
   tags = local.etiquetas
